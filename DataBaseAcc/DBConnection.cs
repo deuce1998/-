528ec204ad51226a227e46a5dcd8accb59ac2940
Alexey <49403307+deuce1998@users.noreply.github.com>
2021-01-17 12:26:26 +0300

    Добавьте файлы проекта.

---
+++ DataBaseAcc/DBConnection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ресторан.DataBaseAcc
+{
+
+    class DBConnection : IDisposable
+    {
+
+
+        public string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Ресторан.mdb;";
+
+        // поле - ссылка на экземпляр класса OleDbConnection для соединения с БД
+        public OleDbConnection myConnection;
+
+        //Конструктор для создания подключения
+        public DBConnection ()
+        {
+            // создаем экземпляр класса OleDbConnection
+            myConnection = new OleDbConnection(connectString);
+
+            // открываем соединение с БД
+            myConnection.Open();
+
+        }
+
+
+
+       
+
+
+
+        //Деструктор для завершения работы с бд
+        ~DBConnection()
+        {
+            myConnection.Close();
+        }
+
+        public void Dispose()
+        {
+            myConnection.Close();
+           // throw new NotImplementedException();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ресторан
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+       
+        }
+
+        private void Aut_Click(object sender, RoutedEventArgs e)
+        {
+            DataBaseAcc.DBConnection connection = new DataBaseAcc.DBConnection();
+       
+            string query = "SELECT Имя, Отчество FROM Сотрудники WHERE Пароль='" + pass.Text + "'";
+
+            OleDbCommand command = new OleDbCommand(query, connection.myConnection);
+            string name;
+            if (connection.myConnection.State == ConnectionState.Open)
+            {
+               name= command.ExecuteScalar().ToString();
+                MessageBox.Show("Добро пожаловать " + name);
+                Window1 window1 = new Window1();
+                window1.Show();
+                this.Close();
+            }    
+                
+            else
+                throw new Exception("Подключение к базе данных отсутствует!");
+
+            
+
+            connection.Dispose();
+
+        }
+
+
+       
+    }
+}
